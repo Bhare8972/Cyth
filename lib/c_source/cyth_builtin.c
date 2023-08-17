e55e06ee20f8028d0d904e252c800f57a8d10ce3
Bhare8972 <bhare8972@hotmail.com>
2023-08-17 16:53:27 +0200

    Add files via upload

---
+++ lib/c_source/cyth_builtin.c
@@ -0,0 +1,17 @@
+#include "../c_header/cyth_builtin.h"
+
+void print_int(int I)
+{
+  printf("%d", I);
+}
+
+void print_float(float I)
+{
+  printf("%f", I);
+}
+
+void print_char(int I)
+{
+  printf("%c", I);
+}
+
